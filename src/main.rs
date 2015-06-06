ec55e8a1d453cc867af9db0c15e7a49229241d58
kloerhe <kloer.he@skywatch24.com>
2015-06-06 22:24:35 +0800

    use serialization lib instead.

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,62 @@
 #![feature(core)]
+#![feature(custom_derive, plugin)]
 extern crate curl;
+extern crate rustc_serialize;
 extern crate ini;
+use rustc_serialize::json;
 
 use curl::http;
 use std::str;
 use std::string;
 use ini::Ini;
+use std::collections::BTreeMap;
+
+#[derive(RustcDecodable, RustcEncodable)]
+struct Profile {
+  //first_name: String,
+  //last_name: String,
+  real_name: String,
+  real_name_normalized: String,
+  email: String,
+  image_24: String,
+  image_32: String,
+  image_48: String,
+  image_72: String,
+  image_192: String
+}
+
+#[derive(RustcDecodable, RustcEncodable)]
+struct User {
+  id: String,
+  name: String,
+  deleted: bool,
+  status: String,
+  color: String,
+  real_name: String,
+  tz: String,
+  tz_label: String,
+  tz_offset: i32,
+  profile: Profile,
+  is_admin: bool,
+  is_owner: bool,
+  is_primary_owner: bool,
+  is_restricted: bool,
+  is_ultra_restricted: bool,
+  is_bot: bool,
+  has_files: bool,
+  has_2fa: bool
+}
+
+#[derive(RustcDecodable, RustcEncodable)]
+struct Users {
+  ok: bool,
+  members: Vec<User>
+}
 
 struct slack_api_info
 {
   token: String,
-  channel: String  
+  channel: String
 }
 
 fn print_type_of<T>(_: &T) -> () {
@@ -43,7 +89,11 @@ fn get_http_content<'a>(url: &'a str) -> String {
 
 pub fn main() {
   let api_conf = get_slack_api_conf("slack.conf");
-  let user_list_api = format!("https://slack.com/api/users.list?token={}&pretty=1", api_conf.token);
+  let mut user_list_api = format!("https://slack.com/api/users.list?token={}&pretty=1", api_conf.token);
+  user_list_api = format!("http://114.44.248.103:8000/json.txt");
   let content = get_http_content(user_list_api.trim());
   println!("result is {}", content);
+  // println!("{}", content.trim());
+  let users : Users = json::decode(&content.trim()).unwrap();
+  // println!("data: {:?}", data);
 }
